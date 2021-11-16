fec874e94c69209ff98f906aca213e79bbe81823
sherlyfebrianti96 <sherlyfebrianti96@yahoo.co.id>
2021-11-16 21:57:09 +0100

    Solution for React Slideshow App

--- react-slideshow-app/src/components/Slides.js
+++ react-slideshow-app/src/components/Slides.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Slides({slides}) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const onRestart = () => {
+    setCurrentSlide(0);
+  };
+
+  const onPrev = () => {
+    setCurrentSlide(currentSlide - 1);
+  };
+
+  const onNext = () => {
+    setCurrentSlide(currentSlide + 1);
+  };
+
+  const firstItem = currentSlide === 0;
+  const lastItem = currentSlide === (slides.length - 1);
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button
+                  data-testid="button-restart"
+                  className="small outlined"
+                  disabled={firstItem}
+                  onClick={onRestart}
+                >
+                  Restart
+                </button>
+                <button
+                  data-testid="button-prev"
+                  className="small"
+                  disabled={firstItem}
+                  onClick={onPrev}
+                >
+                  Prev
+                </button>
+                <button
+                  data-testid="button-next"
+                  className="small"
+                  disabled={lastItem}
+                  onClick={onNext}
+                >
+                  Next
+                </button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[currentSlide].title}</h1>
+                <p data-testid="text">{slides[currentSlide].text}</p>
             </div>
         </div>
     );
